c92981b9dcd611e1e0c4ec00e3b6e2733d1894e6
Chase Crawford <ccrawford@assimalign.com>
2022-02-14 21:07:16 -0500

    Updates 2022-02-14 09:07 PM EST

--- modules.schemas.push.ps1
+++ modules.schemas.push.ps1
@@ -1,7 +1,27 @@
 
+$storageAccountName = 'asalschemastore'
+$storageAccountContainerName = 'json'
+$storageAccountResourceGroup = 'asal-main-rg-est-us-01'
 
 
+Connect-AzAccount
 
-$container = Get-AzStorageContainer -Name ''
+$account = Get-AzStorageAccount -ResourceGroupName $storageAccountResourceGroup -Name $storageAccountName 
+$items = Get-ChildItem './src' -Recurse -Include '*.json'
+$items | ForEach-Object {
+   
+    $paths = $_.DirectoryName.Split('\')
+    $version = $paths[$paths.Length - 1]
 
-$container.BlobContainerClient.
+    if ($version -match "^v(\d*\.\d)") {
+
+        $index = $_.FullName.IndexOf('schemas') + 'schemas'.Length + 1
+        $path =  $_.FullName.Substring($index , $_.FullName.Length - $index)
+        $response = Set-AzStorageBlobContent `
+            -Container $storageAccountContainerName `
+            -Context $account.Context `
+            -Blob "bicep\$path" `
+            -File $_.FullName `
+            -Force
+    }   
+}
