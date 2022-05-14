14a4124750665e8bc049a07a86a363f578fbbf43
Chase Crawford <ccrawford@eastdilsecured.com>
2022-05-14 14:02:39 -0400

    Updates

--- modules.schemas.push.ps1
+++ modules.schemas.push.ps1
@@ -1,29 +1,25 @@
 param (
-    [string]$storageAccountName
+    [string]$storageAccountName,
+    [string]$storageAccountResourceGroup,
+    [string]$storageAccountContainerName
 )
 
-Write-Host $storageAccountName -ForegroundColor Green
-# $storageAccountResourceGroup = "$(azure-schema-stgact-rg)"
-# $storageAccountContainerName = 'json'
-
-# Install-Module Az -Force -AllowClobber
-
-# $account = Get-AzStorageAccount -ResourceGroupName $storageAccountResourceGroup -Name $storageAccountName 
-# $items = Get-ChildItem './src' -Recurse -Include '*.json'
-# $items | ForEach-Object {
+$account = Get-AzStorageAccount -ResourceGroupName $storageAccountResourceGroup -Name $storageAccountName 
+$items = Get-ChildItem './src' -Recurse -Include '*.json'
+$items | ForEach-Object {
    
-#     $paths = $_.DirectoryName.Split('\')
-#     $version = $paths[$paths.Length - 1]
+    $paths = $_.DirectoryName.Split('\')
+    $version = $paths[$paths.Length - 1]
 
-#     if ($version -match "^v(\d*\.\d)") {
+    if ($version -match "^v(\d*\.\d)") {
 
-#         $index = $_.FullName.IndexOf('schemas') + 'schemas'.Length + 1
-#         $path =  $_.FullName.Substring($index , $_.FullName.Length - $index)
-#         $response = Set-AzStorageBlobContent `
-#             -Container $storageAccountContainerName `
-#             -Context $account.Context `
-#             -Blob "bicep\$path" `
-#             -File $_.FullName `
-#             -Force
-#     }   
-# }
+        $index = $_.FullName.IndexOf('schemas') + 'schemas'.Length + 1
+        $path =  $_.FullName.Substring($index , $_.FullName.Length - $index)
+        $response = Set-AzStorageBlobContent `
+            -Container $storageAccountContainerName `
+            -Context $account.Context `
+            -Blob "bicep\$path" `
+            -File $_.FullName `
+            -Force
+    }   
+}
