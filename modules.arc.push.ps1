615e8157c91e085d986ec86b2300facfdeb1000e
Chase Crawford <ccrawford@eastdilsecured.com>
2022-05-14 13:52:05 -0400

    Updates

--- modules.arc.push.ps1
+++ modules.arc.push.ps1
@@ -1,17 +1,17 @@
-$arcurl = "$(azure-arc-fqd)"
-$items = Get-ChildItem './src' -Recurse -Include '*.bicep'
-$items | ForEach-Object {
+# $arcurl = "$(azure-arc-fqd)"
+# $items = Get-ChildItem './src' -Recurse -Include '*.bicep'
+# $items | ForEach-Object {
    
-    $paths = $_.DirectoryName.Split('/')
-    $version = $paths[$paths.Length - 1]
+#     $paths = $_.DirectoryName.Split('/')
+#     $version = $paths[$paths.Length - 1]
 
-    if ($version -match "^v(/d*/./d)") {
+#     if ($version -match "^v(/d*/./d)") {
 
-        $moduleName = $_.BaseName
-        $modulePath = "br:$arcurl/modules/$moduleName" + ":" + $version
+#         $moduleName = $_.BaseName
+#         $modulePath = "br:$arcurl/modules/$moduleName" + ":" + $version
         
-        Write-Host "Uploading $modulePath"
-        Publish-AzBicepModule -FilePath $_.FullName -Target $modulePath
-    }   
-}
+#         Write-Host "Uploading $modulePath"
+#         Publish-AzBicepModule -FilePath $_.FullName -Target $modulePath
+#     }   
+# }
 
--- modules.schemas.push.ps1
+++ modules.schemas.push.ps1
@@ -1,26 +1,28 @@
 
-$storageAccountName = "$(azure-schema-stgact)"
-$storageAccountResourceGroup = "$(azure-schema-stgact-rg)"
-$storageAccountContainerName = 'json'
+$storageAccountName = $AzureSchemaStgAct
 
-Install-Module Az -Force -AllowClobber
+Write-Host $storageAccountName -ForegroundColor Green
+# $storageAccountResourceGroup = "$(azure-schema-stgact-rg)"
+# $storageAccountContainerName = 'json'
 
-$account = Get-AzStorageAccount -ResourceGroupName $storageAccountResourceGroup -Name $storageAccountName 
-$items = Get-ChildItem './src' -Recurse -Include '*.json'
-$items | ForEach-Object {
+# Install-Module Az -Force -AllowClobber
+
+# $account = Get-AzStorageAccount -ResourceGroupName $storageAccountResourceGroup -Name $storageAccountName 
+# $items = Get-ChildItem './src' -Recurse -Include '*.json'
+# $items | ForEach-Object {
    
-    $paths = $_.DirectoryName.Split('\')
-    $version = $paths[$paths.Length - 1]
+#     $paths = $_.DirectoryName.Split('\')
+#     $version = $paths[$paths.Length - 1]
 
-    if ($version -match "^v(\d*\.\d)") {
+#     if ($version -match "^v(\d*\.\d)") {
 
-        $index = $_.FullName.IndexOf('schemas') + 'schemas'.Length + 1
-        $path =  $_.FullName.Substring($index , $_.FullName.Length - $index)
-        $response = Set-AzStorageBlobContent `
-            -Container $storageAccountContainerName `
-            -Context $account.Context `
-            -Blob "bicep\$path" `
-            -File $_.FullName `
-            -Force
-    }   
-}
+#         $index = $_.FullName.IndexOf('schemas') + 'schemas'.Length + 1
+#         $path =  $_.FullName.Substring($index , $_.FullName.Length - $index)
+#         $response = Set-AzStorageBlobContent `
+#             -Container $storageAccountContainerName `
+#             -Context $account.Context `
+#             -Blob "bicep\$path" `
+#             -File $_.FullName `
+#             -Force
+#     }   
+# }
